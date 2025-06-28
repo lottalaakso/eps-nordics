a5c200e3334b663380b63e8de5fb18f72ef13f92
lottalaakso <lottaolivia@gmail.com>
2025-06-28 13:25:14 +0300

    updated version

--- slideshow.js
+++ slideshow.js
@@ -14,11 +14,11 @@ function showSlides(n) {
   }
 
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" select", "");
   }
 
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  dots[slideIndex - 1].className += " select";
 }
 
 function plusSlides(n) {
